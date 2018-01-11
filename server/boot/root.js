f46133e78cf8c6f28859114536fda928be5daeeb
Daniel Prado <danazkari@gmail.com>
2018-01-10 18:15:03 -0600

    initial commit with working react app in client folder

---
+++ server/boot/root.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/status', server.loopback.status());
+  router.get('/', (req, res) => {
+    var indexFile = path.resolve(__dirname, '../..', server.get('indexFile'));
+    res.sendFile(indexFile);
+  });
+  server.use(router);
+};
